12fc45faf9365b643414427ebe77884d0ca3eac5
Victor <vovovkaba@gmail.com>
2023-10-30 23:57:32 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+
+namespace Hello_the_world
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Anung pangalan mo Yawa??: ");
+            const double pi = Math.PI;
+                string word = Console.ReadLine();
+                if (word == "Ann")
+                {
+                    Console.WriteLine("Apaka Ambot mo " + word+ "!!!!");
+                } else if (word=="Victor"){
+                    Console.WriteLine("Ang pogi mo " + word);
+                }
+                else {
+                    Console.WriteLine("Cnu ka " + word +"??");
+               }
+
+        }
+
+
+    }
+}
