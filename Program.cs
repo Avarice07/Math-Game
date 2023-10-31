830f0a5537dbcc33a709aeb2c83acb1dc959eb99
Victor <vovovkaba@gmail.com>
2023-10-31 04:20:34 -0700

    Added the rest of the features

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
+using static System.Formats.Asn1.AsnWriter;
+using System.Text;
 
 namespace Hello_the_world
 {
@@ -27,57 +29,59 @@ namespace Hello_the_world
 
             void OptionKey()
             {   
-                Random a = new Random();
+               
                 bool t = true;
-                int round = 0;
-                int score = 0;
 
-                while(t) {
+                while (t) {
                     char option = Console.ReadKey(true).KeyChar;
                     if (option == 'A' || option == 'a')
                     {
-                        while (round < 5)
-                        {
-                            Console.Clear();
-                            int x = a.Next(101);
-                            int y = a.Next(101);
-                            Console.WriteLine($"{x} + {y} = ?");
-                            try
-                            {
-                                int answer = int.Parse(Console.ReadLine().Trim());
-                                if (answer == x + y)
-                                {
-                                    Console.WriteLine("Correct!!!");
-                                    score += 1;
-                                    AnyKey(1);
-
-                                }
-                                else
-                                {
-                                    Console.WriteLine("Wrong Answer!!");
-                                    AnyKey(1);
-
-                                }
-
-                            }
-                            catch (Exception e)
-                            {
-                                Console.WriteLine("Wrong Value");
-                                AnyKey(1);
-                            }
-
-
-                            round += 1;
-
-
-                        }
-                        player.playerRecord.Add(new Record(score, DateTime.Now));
+                        MathRound(1);
                         t = false;
-                        Console.WriteLine($"You got {score} out of the 5 correct!!!");
                         AnyKey(1);
                         Menu();
 
 
+                    }
+                    else if (option == 'S' || option == 's')
+                    {
+                        MathRound(2);
+                        t = false;
+                        AnyKey(1);
+                        Menu();
+
+
+                    }
+                    else if (option == 'M' || option == 'm')
+                    {
+                        MathRound(3);
+                        t = false;
+                        AnyKey(1);
+                        Menu();
+
+
+                    }
+                    else if (option == 'S' || option == 's')
+                    {
+                        MathRound(4);
+                        t = false;
+                        AnyKey(1);
+                        Menu();
+
+
+                    }
+                    else if (option == 'V' || option == 'v')
+                    {   
+                        Console.Clear();
+                        Console.WriteLine("          ***Players Game Record***");
+                        Console.WriteLine("-----------------------------------------------");
+                        foreach (Record r in player.playerRecord) {
+                           Console.WriteLine($"Played at {r.date} with a score of {r.points} points");
+                        }
+                        Console.WriteLine("-----------------------------------------------");
+                        Console.WriteLine($"The total score for {player.name} is {player.score}");
+                        AnyKey(1);
+                        Menu();
                     }
                     else if (option == 'Q' || option == 'q') 
                     {
@@ -92,9 +96,89 @@ namespace Hello_the_world
 
             }
 
-            void MathRound()
-            { 
-            
+            void MathRound(int o)
+            {
+                
+                Random a = new Random();
+                int round = 0;
+                int score = 0;
+                while (round < 5)
+                {
+                    Console.Clear();
+                    int x = a.Next(101);
+                    int y = a.Next(101);
+                    Console.WriteLine($"{x} {OpChoice(o)} {y} = ?");
+                    try
+                    {
+                        int answer = int.Parse(Console.ReadLine().Trim());
+
+                        if (answer == OpCond(o, x, y))
+                        {
+                            Console.WriteLine("Correct!!!");
+                            score += 1;
+                            AnyKey(1);
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("Wrong Answer!!");
+                            AnyKey(1);
+
+                        }
+
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Wrong Value");
+                        AnyKey(1);
+                    }
+
+
+                    round += 1;
+
+
+                }
+                player.playerRecord.Add(new Record(score, DateTime.Now));
+                Console.WriteLine($"You got {score} out of the 5 correct!!!");
+                player.score += score;
+
+            }
+
+            string OpChoice(int o) {
+
+                switch (o)
+                {
+                    case 1:
+                        return "+";
+                    case 2:
+                        return "-";
+                    case 3:
+                        return "*";
+                    case 4:
+                        return "/";
+                    default:
+                        return " ";
+                }
+                    
+           }
+
+            int OpCond(int o,int x,int y)
+            {
+
+                switch (o)
+                {
+                    case 1:
+                        return x + y;
+                    case 2:
+                        return x - y;
+                    case 3:
+                        return x * y;
+                    case 4:
+                        return x / y;
+                    default:
+                        return 0;
+                }
+
             }
 
             void Menu() 
